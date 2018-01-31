623f2f547b8d9cd9a13a211ecd3303eeb2d32853
DESKTOP-1NFIFHB\Joshua <joshlee_1996@hotmail.com>
2018-01-31 23:48:16 +0800

    -completed addressbook-level1 [LO-MethodAbstraction]

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -994,12 +994,12 @@ public class AddressBook {
 
         // phone is last arg, target is from prefix to end of string
         if (indexOfPhonePrefix > indexOfEmailPrefix) {
-            return removePrefixSign(encoded.substring(indexOfPhonePrefix, encoded.length()).trim(),
+            return removeParamPrefix(encoded.substring(indexOfPhonePrefix, encoded.length()).trim(),
                     PERSON_DATA_PREFIX_PHONE);
 
         // phone is middle arg, target is from own prefix to next prefix
         } else {
-            return removePrefixSign(
+            return removeParamPrefix(
                     encoded.substring(indexOfPhonePrefix, indexOfEmailPrefix).trim(),
                     PERSON_DATA_PREFIX_PHONE);
         }
@@ -1017,12 +1017,12 @@ public class AddressBook {
 
         // email is last arg, target is from prefix to end of string
         if (indexOfEmailPrefix > indexOfPhonePrefix) {
-            return removePrefixSign(encoded.substring(indexOfEmailPrefix, encoded.length()).trim(),
+            return removeParamPrefix(encoded.substring(indexOfEmailPrefix, encoded.length()).trim(),
                     PERSON_DATA_PREFIX_EMAIL);
 
         // email is middle arg, target is from own prefix to next prefix
         } else {
-            return removePrefixSign(
+            return removeParamPrefix(
                     encoded.substring(indexOfEmailPrefix, indexOfPhonePrefix).trim(),
                     PERSON_DATA_PREFIX_EMAIL);
         }
@@ -1151,13 +1151,26 @@ public class AddressBook {
     /**
      * Removes sign(p/, d/, etc) from parameter string
      *
-     * @param s  Parameter as a string
-     * @param sign  Parameter sign to be removed
+     * @param param  Parameter as a string
+     * @param prefix  Parameter sign to be removed
      * @return  string without the sign
      */
-    private static String removePrefixSign(String s, String sign) {
-        return s.replace(sign, "");
+    private static String removeParamPrefix(String param, String prefix) {
+        return removePrefix(param, prefix);
     }
+
+
+    /**
+     * Removes prefix from the given fullString if prefix occurs at the start of the string.
+     */
+    private static String removePrefix  (String fullString, String prefix) {
+        if (fullString.startsWith(prefix)) {
+            return fullString.replace(prefix, "");
+        } else {
+            return fullString;
+        }
+    }
+
 
     /**
      * Splits a source string into the list of substrings that were separated by whitespace.
