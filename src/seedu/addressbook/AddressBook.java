d8ff99dd94f48607ae5a7c00f7d75e5a16f6f536
DESKTOP-1NFIFHB\Joshua <joshlee_1996@hotmail.com>
2018-01-31 23:45:09 +0800

    -AddressBook.java: changed switch form in executeCommand method -AddressBook.java: fix line breaking of ternary statement in executeDeletePerson method

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -510,8 +510,9 @@ public class AddressBook {
             return MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
         }
         final String[] targetInModel = getPersonByLastVisibleIndex(targetVisibleIndex);
-        return deletePersonFromAddressBook(targetInModel) ? getMessageForSuccessfulDelete(targetInModel) // success
-                                                          : MESSAGE_PERSON_NOT_IN_ADDRESSBOOK; // not found
+        return deletePersonFromAddressBook(targetInModel)
+                ? getMessageForSuccessfulDelete(targetInModel) // success
+                : MESSAGE_PERSON_NOT_IN_ADDRESSBOOK; // not found
     }
 
     /**
